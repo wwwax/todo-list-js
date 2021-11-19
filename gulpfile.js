44c042ee2ae7e5295d258d322980fda06e4548e7
Max Ivanov <waxivanov@gmail.com>
2021-11-19 14:26:21 +0200

    css-fn

--- gulpfile.js
+++ gulpfile.js
@@ -30,6 +30,11 @@ const browser_sync = require("browser-sync").create();
 const del = require("del");
 const { src, dest } = require("gulp");
 const gulp = require("gulp");
+const scss = require("gulp-sass")(require("sass"));
+const group_media = require("gulp-group-css-media-queries");
+const autoprefixer = require("gulp-autoprefixer");
+const clean_css = require("gulp-clean-css");
+const rename = require("gulp-rename");
 
 // ################
 
@@ -53,15 +58,38 @@ function html() {
     .pipe(browser_sync.stream());
 }
 
+function css() {
+  return src(path.src.css)
+    .pipe(scss({ outputStyle: "expanded" }).on("error", scss.logError))
+    .pipe(group_media())
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ["last 5 versions"],
+        cascade: true,
+      })
+    )
+    .pipe(dest(path.build.css))
+    .pipe(clean_css())
+    .pipe(
+      rename({
+        extname: ".min.css",
+      })
+    )
+    .pipe(dest(path.build.css))
+    .pipe(browser_sync.stream());
+}
+
 function watchFiles() {
   gulp.watch([path.watch.html], html);
+  gulp.watch([path.watch.css], css);
 }
 
 // ################
 
-const build = gulp.series(clean, gulp.parallel(html));
+const build = gulp.series(clean, gulp.parallel(html, css));
 const watch = gulp.parallel(build, watchFiles, browserSync);
 
 exports.html = html;
+exports.css = css;
 exports.clean = clean;
 exports.default = watch;
